6e28837c58e0ada60aa4b1ee6f31d2f5e69b39bf
Siva-chanthuru <sivachanthuru97@gmail.com>
2026-04-28 22:46:00 +0530

    nodejs first cls

---
+++ First.js
@@ -0,0 +1,15 @@
+console.log("welcome to javascript");
+console.log("welcome to javascript");
+console.log("welcome to javascript");
+console.log("welcome to javascript");
+console.log("welcome to javascript");
+
+let name = "Sivachanthuru";
+let fname = "Siva";
+let lname = "Chanthuru";
+let dob = "05.05.1997";
+
+console.log("My name is " + name);
+console.log("Father name is " + fname);
+console.log("Last name is " + lname);
+console.log("Date of Birth is " + dob);
